4e098fea2b0fb025760ae88582178b6ee6060852
Rasmus Herlo <59374684+RasHerlo@users.noreply.github.com>
2024-02-03 13:31:52 -0500

    Expanding on files

--- bPAC_Neu_DataProcessing_new_240202.m
+++ bPAC_Neu_DataProcessing_new_240202.m
@@ -16,3 +16,20 @@ ENTER:
 
 rtdir = 'D:\bPAC Prairie1\240201 bPAC AP';
 
+ExpRounds = dir(rtdir);
+ExpRounds = ExpRounds(3:end);
+
+% Start with round 4:
+% OBS: Where FaceCam is added (!)
+
+% Manually:
+ExpDir = fullfile(ExpRounds(4).folder,ExpRounds(4).name);
+% alt: use uiggetdir
+
+[file, path] = uigetfile('*.mat','Select the s2p output file:','Fall.mat');
+load(fullfile(path,file))
+
+%% Visualize the and mark/label the individual ROIs and traces
+
+
+
