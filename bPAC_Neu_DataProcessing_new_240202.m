8760707f76f2fb9630712023e97e66f0d3a0df83
Rasmus Herlo <59374684+RasHerlo@users.noreply.github.com>
2024-02-03 13:18:18 -0500

    Uploading v1 Neu_processing
    Starting the branch with neural processing.
    
    Identifying ROIs with interesting signal patterns

---
+++ bPAC_Neu_DataProcessing_new_240202.m
@@ -0,0 +1,18 @@
+
+%% Corresponding Neuron-processing to AC-activity
+
+
+%{
+ENTER: 
+- Stimulation TimeZones
+    - AirPuffs
+    - 1P-stim
+    - LED-stim
+- Total Time
+- Number of frames
+%}
+
+%% OPTION 1: Taken directly from Fall.mat files
+
+rtdir = 'D:\bPAC Prairie1\240201 bPAC AP';
+
